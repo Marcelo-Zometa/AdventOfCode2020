8050df2cbafe1ce4c2051a76f4b716de25c8a4e3
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-12-02 02:35:54 -0700

    Day 1 challenges passed

---
+++ Day 1/ReportRepair/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ReportRepair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Report report = new Report();
+
+            try
+            {
+                //var productOfTwoNumbers = report.twoNumbersThatAddTo2020();
+                //Console.WriteLine("Product of number is " + productOfTwoNumbers);
+
+                var productOfThreeNumbers = report.threeNumbersThatAddTo2020();
+                Console.WriteLine("Product of number is " + productOfThreeNumbers);
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Day 1/ReportRepair/Report.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReportRepair
+{
+    public class Report
+    {
+        public List<int> ExpensesList { get; set; }
+
+        public Report()
+        {
+            ExpensesList = new List<int>();
+            getExpenses();
+        }        
+
+        public void getExpenses()
+        {
+            var path = "C:/Users/engr/source/repos/AdventOfCode2020/Day 1/ReportRepair/input.txt";
+            try
+            {
+                var input = File.ReadAllLines(path);
+                
+                foreach(string s in input)
+                {
+                    ExpensesList.Add(Int32.Parse(s));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File couldn't be read");
+            }
+        }
+      
+        public int twoNumbersThatAddTo2020()
+        {
+            foreach(int num1 in ExpensesList)
+            {
+                foreach(int num2 in ExpensesList)
+                {
+                    if (num1 + num2 == 2020)
+                    {
+                        Console.WriteLine($"Numbers are {num1} and {num2}");
+                        return num1 * num2;                        
+                    }
+                }
+            }
+
+            throw new InvalidOperationException("Numbers not found");
+        }
+
+        public int threeNumbersThatAddTo2020()
+        {
+            foreach (int num1 in ExpensesList)
+            {
+                foreach (int num2 in ExpensesList)
+                {
+                    foreach(int num3 in ExpensesList)
+                    {
+                        if (num1 + num2 + num3 == 2020)
+                        {
+                            Console.WriteLine($"Numbers are {num1}, {num2} and {num3}");
+                            return num1 * num2 * num3;
+                        }
+                    }
+                }
+            }
+
+            throw new InvalidOperationException("Numbers not found");
+        }
+    }
+}
